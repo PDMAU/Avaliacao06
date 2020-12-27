d9f884c01e068a38485a3692e500f3ccc4953e4a
pdmau <maurilio032@gmail.com>
2020-12-27 19:57:49 -0300

    [RED] Criando lista

--- ArquivosRecentes/test/RecentFilesTest.java
+++ ArquivosRecentes/test/RecentFilesTest.java
@@ -3,6 +3,11 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class RecentFilesTest {
-
+	
+	@Test
+	public void quandoaAListaEhCriadaDeveEstarVazia() {
+		RecentFiles lista = new RecentFiles();
+		assertTrue(lista.getLista().isEmpty());
+	}
 	
 }
