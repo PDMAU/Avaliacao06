3aa069ab73aeb229d6e5b4645ce87b35f20aabd8
pdmau <maurilio032@gmail.com>
2020-12-27 20:07:24 -0300

    [GREEN] Criando getLista

--- ArquivosRecentes/src/RecentFiles.java
+++ ArquivosRecentes/src/RecentFiles.java
@@ -1,4 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class RecentFiles {
+	
+	
+	public RecentFiles() {
+	}
 
+	public List<String> getLista(){
+		return new ArrayList<String>();
+	}
 }
