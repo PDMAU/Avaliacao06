60ce253d29c6adb8f25cb74925ee8ef3b36b0857
pdmau <maurilio032@gmail.com>
2020-12-27 20:13:55 -0300

    [RED]Adicionando elementos na lista

--- ArquivosRecentes/test/RecentFilesTest.java
+++ ArquivosRecentes/test/RecentFilesTest.java
@@ -5,9 +5,16 @@ import org.junit.jupiter.api.Test;
 class RecentFilesTest {
 	
 	@Test
-	public void quandoaAListaEhCriadaDeveEstarVazia() {
+	public void quandoAListaEhCriadaDeveEstarVazia() {
 		RecentFiles lista = new RecentFiles();
 		assertTrue(lista.getLista().isEmpty());
 	}
 	
+	@Test
+	public void quandoArquivoEhAdicionadoNaListaONumeroDeElementosDeveAumentar() {
+		RecentFiles lista = new RecentFiles();
+		lista.adicionarArquivo("NovoArquivo.txt");
+		assertEquals(1, lista.getLista().size());
+		
+	}
 }
